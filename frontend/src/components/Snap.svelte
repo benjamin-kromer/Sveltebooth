5371b8791a6727e592748fd09a31f27c4d281270
benjamin-kromer <benjamin.kromer1986@gmail.com>
2023-04-07 23:16:11 +0200

    created a auth path in the API. (Maybe magic link in a QR Code with JWT Token as query param in the link?). Also started the main functionality taking photos..

--- frontend/src/components/Snap.svelte
+++ frontend/src/components/Snap.svelte
@@ -1,10 +1,25 @@
 <script>
-	const takePicture = ()=> {
-		console.log("taking picture")
-	}
+	import { onMount } from 'svelte';
+	
+	let camera;
+	const startVideoStream = async () => {
+		try {
+			const videoStream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+			camera.srcObject = videoStream;
+			camera.play();
+		} catch (err) {
+			console.log(err);
+		}
+		const takePicture = () => {
+			console.log('taking picture');
+		};
+	};
+	onMount(startVideoStream);
 </script>
 
 <div>
 	<h1>Snap View</h1>
-	<button class="ui button massive red" on:click={takePicture}>SNAP</button>
+	<!-- svelte-ignore a11y-media-has-caption -->
+	<video bind:this={camera} />
+		<button class="ui button massive red" on:click={()=> console.log("takePicture")}>SNAP</button>
 </div>
