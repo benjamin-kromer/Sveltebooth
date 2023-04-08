201e227a1255ac33e1367f99a9d4a8d2adda3458
benjamin-kromer <benjamin.kromer1986@gmail.com>
2023-04-08 22:53:04 +0200

    changed menu. working on auth mechanic

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <nav>
 	<div class="ui bottom fixed menu">
-		<a class="ui button black" href="/">Home</a>
-		<a class="ui button green" href="/gallery">Gallery</a>
-		<a class="ui button red" href="/snap">Snap</a>
+		<a class="item" href="/">Home</a>
+		<a class="item" href="/gallery">Gallery</a>
+		<a class="item" href="/snap">Snap</a>
 	</div>
 </nav>
 
--- frontend/src/routes/api/v1/auth/+server.js
+++ frontend/src/routes/api/v1/auth/+server.js
@@ -1,11 +1,12 @@
-//import { error, json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
+import { SECRET } from '$env/static/private';
 import jwt from 'jsonwebtoken';
 import { redirect } from '@sveltejs/kit';
 export const GET = async ({ url }) => {
     const token = String(url.searchParams.get('token') ?? "");
-    console.log(token)
-    if (token === "hush") {
-        return new Response(JSON.stringify({ "ok": "good" }), { status: 200 });
+    if (token === SECRET) {
+        //return new Response(JSON.stringify({ "ok": "good" }), { status: 200 });
+        throw redirect(307,'/login')
        
     } else {
         throw redirect(307, '/');
